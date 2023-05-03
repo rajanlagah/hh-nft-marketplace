b4f78da2d5a2fc564c5d7e3dddad6ffd36911664
lagahrajan <rajan.lagah@swymcorp.com>
2023-05-03 21:47:00 +0530

    connect btn WIP

---
+++ moralis-nextjs/components/Headers.js
@@ -0,0 +1,6 @@
+import { ConnectButton } from "web3uikit";
+
+const Header = () => {
+    return <ConnectButton />;
+};
+export default Header;
--- moralis-nextjs/pages/_app.js
+++ moralis-nextjs/pages/_app.js
@@ -1,9 +1,12 @@
-import "@/styles/globals.css";
 import { MoralisProvider } from "react-moralis";
+
+import "@/styles/globals.css";
+import Headers from "@/components/Headers";
 
 export default function App({ Component, pageProps }) {
     return (
         <MoralisProvider initializeOnMount={false}>
+            <Headers/>
             <Component {...pageProps} />
         </MoralisProvider>
     );
